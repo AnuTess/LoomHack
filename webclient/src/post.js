a357ff9384db76b5a00d78fb5903d86e546ede5b
ltejedor <lmtejedor@gmail.com>
2018-08-11 17:42:25 -0400

    add emojis!

--- webclient/src/post.js
+++ webclient/src/post.js
@@ -22,6 +22,16 @@ export default class Post extends React.Component {
     return (
       <div>
         <pre>{this.props.value.text}</pre>
+        <div>
+          <small>
+            <span>this makes me feel: </span>
+            <span class="emoji-selection" role="img" aria-label="happy">😀</span>
+            <span class="emoji-selection" role="img" aria-label="sad">😪</span>
+            <span class="emoji-selection" role="img" aria-label="scared">😱</span>
+            <span class="emoji-selection" role="img" aria-label="lovely">💖</span>
+          </small>
+        </div>
+
         <small>user: {this.props.value.owner}</small>
         <div>
           <button type="button" className="btn btn-primary" onClick={() => this.newComment()} >New Comment</button>
